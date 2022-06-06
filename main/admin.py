6983e8bc8bbdc50a7c8f920ab3fdbed31ad741d1
artem <zalivonka02@gmail.com>
2022-06-06 10:20:26 +0300

    first commit

---
+++ main/admin.py
@@ -0,0 +1,115 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from main.models import *
+# Register your models here.
+
+
+class SecurityAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published')
+    exclude = ('slug',)
+    list_display_links = ('title',)
+    list_editable = ('published',)
+
+
+class CharecteristicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    exclude = ('slug',)
+    list_display = (
+        'title',
+        'price',
+        'price_install',
+        'image_img',
+        'presence',
+        'published',
+        'popular',
+        'novelties'
+    )
+    list_display_links = ('title',)
+    list_editable = ('price', 'price_install', 'presence', 'popular', 'novelties', 'published')
+
+    def image_img(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80" height="80">')
+        else:
+            return 'Нет изображения'
+
+    image_img.short_description = 'Изображение'
+
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'published')
+    list_display_links = ('title',)
+    list_editable = ('published', 'price')
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'sale', 'published')
+    list_display_links = ('title',)
+    list_editable = ('published', 'sale')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'name', 'numbers_phone', 'body_reduction', 'published')
+    list_editable = ('published',)
+    list_filter = ('title', 'name', 'numbers_phone')
+    search_fields = ('title', 'name', 'numbers_phone', 'pub_data', 'body_reduction')
+
+
+class OurWorkAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'installation_time',
+        'installation_price',
+        'image1',
+        'published',
+    )
+    exclude = ('slug',)
+    list_filter = ('title',)
+    list_display_links = ('title',)
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'message', 'published')
+    list_filter = ('published',)
+    list_editable = ('published',)
+
+
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'address',
+        'phone1',
+        'phone2',
+        'email',
+        'time_work1',
+        'time_work2',
+    )
+    list_display_links = ('name',)
+    list_editable = ('time_work1', 'time_work2')
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('description', 'image1', 'image2', 'image3')
+    list_display_links = ('description',)
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Contacts, ContactsAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Action, ActionAdmin)
+admin.site.register(OurWork, OurWorkAdmin)
+admin.site.register(Security, SecurityAdmin)
+admin.site.register(Characteristic, CharecteristicAdmin)
+admin.site.register(Sale, SaleAdmin)
---
+++ main/models.py
@@ -0,0 +1,235 @@
+from ckeditor.fields import RichTextField
+from django.db import models
+from django.utils import timezone
+from slugify import slugify
+# Create your models here.
+
+
+class Security(models.Model):
+    """Охранные комплексы"""
+    title = models.CharField('Название комплекса', max_length=250)
+
+    class Meta:
+        verbose_name = 'Комплекс'
+        verbose_name_plural = 'Комплексы'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """Категории комплексов"""
+    title = models.CharField('Название категории комплекса', max_length=250)
+    slug = models.SlugField('Короткое название', unique=True, db_index=True, max_length=20)
+    security = models.ForeignKey(
+        Security,
+        related_name='categores',
+        on_delete=models.CASCADE,
+        verbose_name='Название комплекса'
+    )
+    published = models.BooleanField('Опубликовано', default=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.title))
+        return super().save(*args, **kwargs)
+
+
+class Characteristic(models.Model):
+    """Характеристика"""
+    title = models.CharField('Название характеристики', max_length=250)
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = 'Характеристика'
+        verbose_name_plural = 'Характеристики'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    """Товар"""
+    PRESENCE_CHOICES = [
+        ["Есть в наличии", "Есть в наличии"],
+        ["Под заказ", "Под заказ"],
+        ["Нет в наличии", "Нет в наличии"]
+    ]
+    title = models.CharField('Название товара', max_length=250)
+    slug = models.CharField('Короткое название', max_length=20, unique=True, db_index=True)
+    description = RichTextField('Описание')
+    price = models.DecimalField('Цена оборудования', decimal_places=2, max_digits=7)
+    price_install = models.DecimalField('Цена установки', decimal_places=2, max_digits=7)
+    image = models.ImageField('Картинка', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='cat', verbose_name='Категория')
+    presence = models.CharField('Наличие товара', max_length=200, choices=PRESENCE_CHOICES)
+    characteristics = models.ManyToManyField(
+        Characteristic,
+        related_name='charecter',
+        blank=True,
+        verbose_name='Характеристики'
+    )
+    instruction = models.FileField('Инструкция', upload_to='file_instruction/%Y/%m/%d/', blank=True, null=True)
+    published = models.BooleanField('Опубликовано', default=True)
+    popular = models.BooleanField('Популярный товар', default=False)
+    novelties = models.BooleanField('Новинка', default=False)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.title))
+        return super().save(*args, **kwargs)
+
+
+class Action(models.Model):
+    """Акции на товар"""
+    title = models.CharField('Название акции', max_length=250)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='prod', verbose_name='Товар на акции')
+    price = models.CharField('Цена со скидкой', max_length=30)
+    published = models.BooleanField('Опубликовано', default=False)
+
+    class Meta:
+        verbose_name = 'Акция на товар'
+        verbose_name_plural = 'Акции на товары'
+
+    def __str__(self):
+        return self.title
+
+
+class Sale(models.Model):
+    """Скидки на установку"""
+    title = models.CharField('Заголовок скидки на монтаж', max_length=250)
+    description = models.TextField('Описание скидки', max_length=250)
+    image = models.ImageField('Изображение', blank=True, null=True, upload_to='sale/')
+    sale = models.CharField('скидка (пример: -30%)', max_length=10)
+    published = models.BooleanField('Опубликовано', default=False)
+
+    class Meta:
+        verbose_name = 'Скидка на установку'
+        verbose_name_plural = 'Скидки на установки'
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Отзыв на работу"""
+    title = models.CharField('Заголовок отзыва', max_length=200, blank=True, null=True)
+    name = models.CharField('Имя отправителя отзыва', max_length=200)
+    numbers_phone = models.CharField('Номер телефона', max_length=20)
+    body = models.TextField('Содержимое комментария')
+    pub_data = models.DateTimeField('Дата комментария', default=timezone.now)
+    published = models.BooleanField('Опубликовано', default=False)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('pub_data',)
+
+    def __str__(self):
+        return f'Отзыв от {self.name} с текстом: {self.body[:20]}'
+
+    def body_reduction(self):
+        return u"%s..." % (self.body[:250],)
+
+    body_reduction.short_description = 'Содержимое комментария'
+
+
+class OurWork(models.Model):
+    """Наши работы"""
+    title = models.CharField('Заголовок', max_length=250)
+    slug = models.CharField('Короткое название', max_length=20, unique=True)
+    category_work = models.ManyToManyField(
+        Category,
+        related_name='category_work',
+        verbose_name='категория установленного оборудования',
+    )
+    installation_time = models.CharField('Время установки', max_length=100)
+    installation_price = models.CharField('Стоимость установки', max_length=100)
+    description_video = models.TextField('Описание видео', blank=True, null=True)
+    url = models.TextField('Видео', help_text='Вставить ссылку с YouTube', blank=True, null=True)
+    description_image = models.TextField('Описание фото', blank=True, null=True)
+    image1 = models.ImageField('Основная картинка', upload_to='image/%Y/%m/%d/')
+    image2 = models.ImageField('Картинка 2', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    image3 = models.ImageField('Картинка 3', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    image4 = models.ImageField('Картинка 4', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    published = models.BooleanField('Опубликовано', default=True)
+
+    class Meta:
+        verbose_name = 'Наши работы'
+        verbose_name_plural = 'Наша работа'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.title))
+        return super().save(*args, **kwargs)
+
+
+class Feedback(models.Model):
+    """Обратная связь"""
+    name = models.CharField('Имя', max_length=100, blank=True, null=True)
+    phone = models.CharField('Номер телефона', max_length=20)
+    message = models.TextField('Опишите свой вопрос', blank=True, null=True)
+    published = models.BooleanField('Обработано', default=False)
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
+
+    def __str__(self):
+        return f'{self.name} : {self.message}'
+
+
+class Contacts(models.Model):
+    """Контакты и информация"""
+    name = models.CharField('Название сервиса', max_length=200)
+    address = models.CharField('Адрес сервиса', max_length=250)
+    phone1 = models.CharField('Номер телефона А1', max_length=50, blank=True, null=True)
+    phone2 = models.CharField('Номер телефона МТС', max_length=50, blank=True, null=True)
+    email = models.CharField('Электронная почта', max_length=200, blank=True, null=True)
+    social_info1 = models.CharField('Социальная сеть VK', max_length=200, blank=True, null=True)
+    social_info2 = models.CharField('Социальная сеть Telegram', max_length=200, blank=True, null=True)
+    time_work1 = models.CharField('Время работы (будни)', max_length=100)
+    time_work2 = models.CharField('Время работы (выходные)', max_length=100)
+    maps = models.TextField(
+        'Расположение на карте',
+        help_text='Вставить ссылку с: https://yandex.ru/map-constructor (выбирать width="100%" height="500")'
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    """О компании"""
+    image1 = models.ImageField('Картинка1', upload_to='image/%Y/%m/%d/')
+    image2 = models.ImageField('Картинка2', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    image3 = models.ImageField('Картинка3', blank=True, null=True, upload_to='image/%Y/%m/%d/')
+    description = RichTextField('Описание')
+
+    class Meta:
+        verbose_name = 'О компании'
+        verbose_name_plural = 'О компании'
+
+    def __str__(self):
+        return self.description
